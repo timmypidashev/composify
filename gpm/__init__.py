a603f03b506b78bbd60ba2c1e5e67baba85ab813
Timothy Pidashev <pidashev.tim@gmail.com>
2022-08-07 20:48:33 -0700

    Minor polish

--- gpm/__init__.py
+++ gpm/__init__.py
@@ -3,7 +3,7 @@ __summary__ = "Git Project Manager(GPM)"
 __author__ = "Timothy Pidashev"
 __license__ = "MIT"
 __copyright__ = "Copyright 2022-present Timothy Pidashev"
-__version__ = "0.1.0-rc.0"
+__version__ = "0.1.0"
 
 
 def run_as_module():
