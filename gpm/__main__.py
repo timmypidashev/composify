21f744a57c9cb5ff4a5c98543cd20c427595f416
Timothy Pidashev <pidashev.tim@gmail.com>
2022-08-07 20:09:43 -0700

    Reconfigure setup

--- gpm/__main__.py
+++ gpm/__main__.py
@@ -2,12 +2,12 @@
 Git Project Manager
 """
 
-__name__ = 'gpm'
-__summary__ = 'Git Project Manager(GPM)'
-__author__ = 'Timothy Pidashev'
-__license__ = 'MIT'
-__copyright__ = 'Copyright 2022-present Timothy Pidashev'
+__name__ = "gpm"
+__summary__ = "Git Project Manager(GPM)"
+__author__ = "Timothy Pidashev"
+__license__ = "MIT"
+__copyright__ = "Copyright 2022-present Timothy Pidashev"
 __version__ = '0.1.0'
 
 def run_as_module():
-    print('gpm test')
+    print('gpm')
