8d404c6ecb060e7e2765795e53ea67bebb23b5f1
Timothy Pidashev <pidashev.tim@gmail.com>
2022-08-08 17:07:14 -0700

    Basic changes; prepare for poetry config

--- install.sh
+++ install.sh
@@ -1,50 +1,49 @@
 #!/bin/bash
-# Note: GPM Installation script
+# Note: GPM Installation script 
+
+# Determine Linux distribution if applicable
+function whichDist {
+
+    # Debian
+    if [[ -f /etc/debian_version ]]; then
+        DIST="Debian"
+    
+    # Arch
+    elif [[ -f /etc/arch-release ]]; then
+        DIST="Arch"
+        
+    else
+        # NOTE: Add more distribution support with time 
+        echo "Unsupported distribution"
+        exit 1
+    fi
+}
+}
 
 # Ensure installing on correct system
-function checkOS {
+function whichOS {
     OS=$(uname)
 
     if [[ $OS == "Linux" ]]; then
-        # Determine linux distro
-
-        # Debian
-        if [[ -f /etc/debian_version ]]; then
-            DIST="Debian"
-
-        # Arch
-        elif [[ -f /etc/arch-release ]]; then
-            DIST="Arch"
-
-        # Fedora
-        elif [[ -f /etc/fedora-release ]]; then
-	        DIST="Fedora"
-
-        # Other
-        else
-            printerr "This distribution is not supported, exiting!"
-            exit
-
-        fi
-
+        whichDist
+    
     elif [[ $OS == "Darwin" ]]; then
-        : # Pass unsupported os trap
-
+        :
+    
     else
-        # NOTE: Consider support for Windows
-        echo "Unsupported OS: $OS"
-        exit
-
+        # NOTE: Add support for Windows in the future 
+        echo "Unsupported OS"
+        exit 1
     fi
 
 }
 
-# Ensure Python is installed
-function checkPython {
+# Ensure python is installed
+function ensurePython {
     :
 }
 
 # Ensure pip is installed
-function checkPip {
+function ensurePip {
     :
 }
