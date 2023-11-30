32e066d405d5938f9ebc916037c5dfea9f2f13b8
timmypidashev <pidashev.tim@gmail.com>
2023-11-29 18:23:57 -0800

    minor changes

--- src/composify/__init__.py
+++ src/composify/__init__.py
@@ -20,59 +20,39 @@ Composify argument defaults dict and
 the user defined arguments dict.
 """
 defaults = {
-    "verbose": False, 
-    "version": False
+    "version": False,
+    "debug": False 
 }
 arguments = dict()
 
-class HelpFormatter(argparse.HelpFormatter):
-    """
-    Custom argparse help formatter. Currently this allows for the following:
-    - Disables metavar for short args. Example without(-t {usb, iso} --type {usb, iso}) | Example with(-t --type {usb, iso})
-    - Allows for extending the default column size for help variables.
-    """
-    def __init__(self, prog):
-        super().__init__(prog, max_help_position=40, width=80)
-
-    def _format_action_invocation(self, action):
-        if not action.option_strings or action.nargs == 0:
-            return super()._format_action_invocation(action)
-        default = self._get_default_metavar_for_optional(action)
-        args_string = self._format_args(action, default)
-        return ', '.join(action.option_strings) + ' ' + args_string
-
-format = lambda prog: HelpFormatter(prog)
-
+# Use the custom formatter
 parser = argparse.ArgumentParser(
-    prog=__title__,
-    usage='%(prog)s [options] [command]',
-    description=f"{__summary__} v{__version__}",
-    formatter_class=format
+    add_help=False  # Disable automatic help display for the main parser
 )
 
 async def define_arguments():
     """
     Define command-line arguments.
     """
-    # options
-    parser.add_argument("-vv", "--verbose", default=defaults["verbose"], help="set composify output to output debug logs", action="store_true") 
-    parser.add_argument('-v', '--version', default=defaults["version"], help='output version information and exit', action='store_true')
+    
+    # commands
+    subparsers = parser.add_subparsers(dest='subcommand')
+    init = subparsers.add_parser("init",)
 
-    # commands subparsers
-    subparsers = parser.add_subparsers(title='commands', dest='subcommand', help='available commands')
-
-    init = subparsers.add_parser("init", help="initialize a new project")
-
+async def help_message():
+    #TODO: Literally print a help command instead of relying on argparse
+    pass
 
 
 async def parse_arguments():
     """
     Parse arguments. If none inputted, default to '--help'.
     """
-    args = parser.parse_args(args=None if sys.argv[1:] else ['--help'])
+    args = parser.parse_args()
 
-    if args.version:
-        print(f'{__title__} v{__version__}')
+    # Remove specific options before displaying help
+    #if args.version:
+    #    print(f'{__title__} v{__version__}')
 
     
 
